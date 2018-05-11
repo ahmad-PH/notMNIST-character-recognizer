7dfd330c8192eb817c9fa033950dde61454b27de
ahmad pourihosseini <seyedahmadpourihosseini@yahoo.com>
2018-05-11 14:39:38 +0430

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+example = ([1] * 28 * 28, 1)
+
+class Example:
+    # label should be index of english letter (e.g. a=0, b=1, ...)
+    def __init__(self, input, label):
+
+
+class Neuron:
+    pass
+
+class Layer:
+    def __init__(self):
+        self.neurons = None
+
+class ANN:
+    def __init__(self):
+        self.layers = None
