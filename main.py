4f5566e300691ee78c1dc998f21a1a3228d8857f
ahmad pourihosseini <seyedahmadpourihosseini@yahoo.com>
2018-05-13 18:37:59 +0430

    Complete update_weights function

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ class Example:
 class Neuron:
     def __init__(self, n_inputs, act_func):
         self.n_inputs = n_inputs + 1 # +1 is for bias
-        self.weights = np.random.normal(0, 0.2, n_inputs + 1)
+        self.weights = np.random.normal(0, 0.1, n_inputs + 1)
         self.intermediate_output = None
         self.output = None
         self.act_func = act_func
+        self.delta = 0 #for backpropagation
 
     def calculate(self, data):
         #temporary, remove after debugging for better performance
@@ -37,6 +38,9 @@ class Neuron:
             result += data[i] * self.weights[i]
         self.intermediate_output = result
         self.output = self.act_func(self.intermediate_output)
+
+    def dropout(self):
+        self.output = 0
 
 class Layer:
     def __init__(self, n_neurons, n_inputs, act_func):
@@ -56,20 +60,28 @@ def sigmoid_derivative(x):
     return exp(-x) / ((1 + exp(-x)) ** 2)
 
 class ANN:
-    def __init__(self, layers_structure, act_function, regularization_type = None, dropout_probabilty):
-        self.act_function = act_function
+    def __init__(self, layers_structure, act_function, regularization_type = None, dropout_probabilty = 0):
+        if act_function == "sigmoid":
+            self.act_function = sigmoid
+            self.act_function_derivative = sigmoid_derivative
+        elif act_function == "linear":
+            pass
+        else:
+            raise RuntimeError("invalid act_function type")
+
+        self.learning_rate = 0.05
+        self._lambda = 0.03
 
         self.layers = []
         num_input = 1
         for layer_size in layers_structure:
-            self.layers.append(Layer(layer_size, 1, act_function))
+            self.layers.append(Layer(layer_size, num_input, act_function))
             num_input = layer_size
 
-        if regularization_type == "L2":
-            pass
-        elif regularization_type == None:
-            pass
+        if regularization_type != "L2" and regularization_type is not None:
+            raise RuntimeError("invalid regularization_type: " + regularization_type)
 
+        self.regularization_type = regularization_type
         self.dropout_probability = dropout_probabilty
 
 
@@ -80,7 +92,10 @@ class ANN:
         for layer in self.layers:
 
             for neuron in layer.neurons:
-                neuron.calculate(current_input)
+                if numpy.random.uniform(0, 1) < self.dropout_probability:
+                    neuron.dropout()
+                else:
+                    neuron.calculate(current_input)
 
             current_input = layer.get_output_all()
             current_input.append(1)
@@ -91,7 +106,7 @@ class ANN:
     def calculate_accuracy(self, data):
         n_correct_answers = 0
         for datum in data:
-            if (self.answer(datum) == datum.label)
+            if self.answer(datum) == datum.label:
                 n_correct_answers += 1
 
         return n_correct_answers / len(data)
@@ -100,11 +115,56 @@ class ANN:
         self.feed_forward(example)
         return max(self.get_output())[0]
 
+    def update_weights(self, example):
 
-    def back_propagate(self):
+        regularization_value = self.calculate_regularization_value()
+        expected_output = self.label_to_expected_output(example.label)
+
+        self.calculate_deltas()
+
+        #for first layer
+        for i, neuron in enumerate(self.layers[0]):
+            neuron.weights[0] += self.learning_rate * (example.input[i] * neuron.delta + self._lambda * neuron.weights[0])
+
+        #for other layers
+        for i, layer in enumerate(self.layers[1:]):
+            for neuron in layer.neurons:
+                for j, weight in enumerate(neuron.weights):
+                    weight += self.learning_rate * ( self.layers[i-1] * neuron.delta + self._lambda + weight)
+
+
+    def calculate_deltas(self):
+        # calculate delta for last layer
+        for i, neuron in enumerate(self.layers[-1].neurons):
+            err = (expected_output[i] - neuron.output) ** 2
+            neuron.delta = (-1) * err * self.act_function_derivative(neuron.intermediate_output)
+
+        # calculate delta for other layers
+        for i in xrange(len(self.layers) - 2, -1, -1):
+            for j, neuron in enumerate(self.layers[i].neurons):
+                sigma = 0
+                for next_layer_neuron in self.layers[i + 1].neurons:
+                    sigma += next_layer_neuron.weights[j] * next_layer_neuron.delta
+                neuron.delta = self.act_function_derivative(neuron.intermediate_output) * sigma
 
 
 
+
+    def calculate_regularization_value(self):
+        if regularization_type == "L2":
+            regularization_value = 0
+            for layer in self.layers:
+                for neuron in layer.neurons:
+                    for weight in neuron.weights:
+                        self.regularization_value += weight ** 2
+        else:
+            regularization_value = 0
+        return regularization_value
+
+    def label_to_expected_output(self, label):
+        result = [0] * len(self.layers[-1])
+        result[label] = 1
+        return result
 
 
 def read_data():
